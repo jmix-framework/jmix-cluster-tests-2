77ac0f295417baa1a28f6d37a8dfa665b6c35098
Dmitry Taimanov <taimanov@haulmont.com>
2023-07-24 19:12:50 +0600

    Hazelcast version has been locked in Jmix

--- reload_local_cluster.sh
+++ reload_local_cluster.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env sh
 
 echo 'Clearing deployment configs...'
-kubectl delete -f ./k8s
+kubectl delete -f ./k8s -n='jmix-cluster-tests'
 echo 'Building app image...'
 ./gradlew bootBuildImage
-echo 'Loading image...'
+echo 'Reloading image...'
+minikube image rm docker.haulmont.com/platform/jmix-kube-tests/sample-cluster:jmix_2_0
 minikube image load docker.haulmont.com/platform/jmix-kube-tests/sample-cluster:jmix_2_0
 echo 'Applying configs...'
-kubectl apply -f ./k8s
+kubectl apply -f ./k8s  -n='jmix-cluster-tests'
 echo 'Done!'
